00842362f7b752dabdab5bb5bb7fce57067df920
Lucas De Oliveira <lucas.deoliveira2112@gmail.com>
2022-07-26 20:32:02 -0300

    changed name of controller

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class TransactionsControllerTest < ActionDispatch::IntegrationTest
+class OrdersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
