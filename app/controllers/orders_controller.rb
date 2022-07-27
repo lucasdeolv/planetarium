c25a57f9df93587a590943daedc0a26260c0529c
murilo finardi <favorettofinardi@gmail.com>
2022-07-27 16:11:19 -0300

    adicionados alguns metodos nos 2 controllers

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,17 @@
 class OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
   end
 
   def show
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit
   end
 end
