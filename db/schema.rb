d290976a371d62f32e2005bfdcca3d0e764ebf31
Lucas De Oliveira <lucas.deoliveira2112@gmail.com>
2022-07-30 15:02:48 -0300

    buy button

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_27_175252) do
+ActiveRecord::Schema.define(version: 2022_07_30_175557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 2022_07_27_175252) do
   create_table "orders", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "planet_id", null: false
+    t.index ["planet_id"], name: "index_orders_on_planet_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "planets", force: :cascade do |t|
@@ -47,5 +51,7 @@ ActiveRecord::Schema.define(version: 2022_07_27_175252) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "planets"
+  add_foreign_key "orders", "users"
   add_foreign_key "planets", "users"
 end
