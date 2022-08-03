99c38b0895afb53f86e3d33878a16ec82e115981
Andre Fonseca <decobf15@gmail.com>
2022-08-03 17:42:53 -0300

    changes on planet.rb

--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,9 +1,5 @@
 class Planet < ApplicationRecord
   belongs_to :user
-  has_many :orders, dependent: :destroy
-  validates :name, :galaxy, :price, :size, :temperature, :pressure, presence: true
 
-  def bought!
-    self.update_attribute(:bought, true)
-  end
+  validates :name, :galaxy, :price, :size, :temperature, :pressure, presence: true
 end
