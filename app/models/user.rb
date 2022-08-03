d83d54931e071297ad97716723e73df390dcd977
Andre Fonseca <decobf15@gmail.com>
2022-08-02 21:34:05 -0300

    o planeta some do index quando comprado

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :planets
-  has_many :orders
+  has_many :planets, dependent: :destroy
+  has_many :orders, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 has_one_attached :photo
